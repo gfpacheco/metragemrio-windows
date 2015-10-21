e538bb1a9f5a09bc37194693763e796a71d0a9b5
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2015-10-21 01:05:36 -0200

    Add meterage model

---
+++ Models/Meterage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetragemRio.Models
+{
+    class Meterage
+    {
+        public DateTime timestamp { get; set; }
+        public Int32 status { get; set; }
+        public Double level { get; set; }
+        public Double precipitation { get; set; }
+    }
+}
